debb57bf668bdd4c9365f0a218753747d0bfdb2b
Brad Roberts <bradr555@gmail.com>
2020-08-31 17:32:25 -0500

    Update ipa.py

--- src/scripts/ipa.py
+++ src/scripts/ipa.py
@@ -5,7 +5,10 @@ import netifaces
 string = str()
 for interface in netifaces.interfaces():
     addrs = netifaces.ifaddresses(interface)
-    for item in addrs[netifaces.AF_INET]:
-        string += f"{interface}: {item.get('addr')}\n"
+    try:
+        for item in addrs[netifaces.AF_INET]:
+            string += f"{interface}: {item.get('addr')}\n"
+    except Exception as e:
+        pass
 
 print(string)
