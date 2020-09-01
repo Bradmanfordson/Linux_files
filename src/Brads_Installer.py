fd61be76642aba773bd22e35d36de2aa23f7a4ae
Brad Roberts <bradr555@gmail.com>
2020-09-01 15:10:05 -0500

    Update Brads_Installer.py

--- src/Brads_Installer.py
+++ src/Brads_Installer.py
@@ -3,10 +3,12 @@ import json
 
 
 whoami = os.popen("whoami").read().strip()
+home = f"/home/{whoami}"
+
 
 def header(message: str): 
     print()
-    print("#" * (4+ len(message)))
+    print("#" * (4 + len(message)))
     print(f"# {message} #")
     print("#" * (4 + len(message)))
 
@@ -18,8 +20,9 @@ def initial_update():
     header("Upgrading")
     os.system("sudo apt upgrade -y")
 
+    
 def setup():
-    os.system(f'PATH="/home/{whoami}/.local/bin:$PATH')
+    os.system(f"echo \"PATH={home}/.local/bin:$PATH\" >> {home}/.bashrc")
 
     with open("settings.json", "r") as json_file:
         data = json.load(json_file)
@@ -28,7 +31,7 @@ def setup():
         for directory in data.get("directories"):
             try:
                 header(f"Creating {directory}")
-                os.mkdir(f"/home/{whoami}/{directory}")
+                os.mkdir(f"{home}/{directory} -p")
             except Exception as e:
                 print(f"Skipping {directory} because it already exists.")
 
@@ -57,19 +60,21 @@ def hakr_toolz():
     os.system("cp -n /opt/exploit-database/.searchsploit_rc ~/")
 
     header("Downloading SecLists")
-    os.system(f"sudo git clone https://github.com/danielmiessler/SecLists.git /home/{whoami}/Tools/SecLists")
-
-    header("Downloading LinPeas and WinPeas")
-    os.system(f"sudo git clone https://github.com/m0nad/awesome-privilege-escalation.git /home/{whoami}/Tools/Peas")
+    os.system(f"sudo git clone https://github.com/danielmiessler/SecLists.git {home}/Tools/SecLists")
     
     header("Downloading and Installing Joplin")
     os.system("wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash")
 
-def aliases():
+
+def add_scripts():
     header("Setting up aliases")
-    os.system(f"cp bash_aliases.txt /home/{whoami}/.bash_aliases")
+    os.system(f"cp bash_aliases.txt {home}/.bash_aliases")
+    
+    header("Setting up IPA")
+    os.system(f"cp scripts/ipa.py {home}/.local/bin/ipa")
+    os.system(f"chmod +x {home}/.local/bin/ipa")
 
-
+    
 def git_setup():
     header("Git setup")
     username = input("Enter your GitHub username: ")
@@ -87,15 +92,17 @@ def git_setup():
 
 if __name__ == "__main__":
     if whoami == "root":
-        print("Don't run this as root.")
+        header("Don't run this as root.")
         exit(1)
+        
     initial_update()
     setup()
-    aliases()
+    add_scripts()
+    
     ans = input("Do you need to set up git? (Y/n): ")
     if ans.lower()[0] == "y":
         git_setup()
+        
     hakr_toolz()
     os.system("sudo updatedb")
     header("Finished")
-
