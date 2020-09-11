f069fdb439f6777c3f9731de88db7b7494090d86
Brad Roberts <bradr555@gmail.com>
2020-09-10 21:59:08 -0500

    Create nmapper.sh

---
+++ src/scripts/nmapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <ipaddress> <outfile>"
+	exit 1
+fi
+
+TARGET=$1
+OUTFILE=$2
+
+nmap -Pn -F $1 -o $2.fast
+nmap -Pn -sV -sC $1 -o $2.sVsC
+nmap -Pn -p- --max-retries 1 $1 -o $2.all
+nmap -Pn -p $(get_ports $2.all) $1 -o $2.sVsC.allports
+
+echo "Done."
