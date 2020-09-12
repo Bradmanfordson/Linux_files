090a453638cb565ca7eb8b900a7ad8eed20178d5
Brad Roberts <bradr555@gmail.com>
2020-09-12 13:40:22 -0500

    Update nmapper.sh

--- src/scripts/nmapper.sh
+++ src/scripts/nmapper.sh
@@ -11,6 +11,6 @@ OUTFILE=$2
 nmap -Pn -F $1 -o $2.fast
 nmap -Pn -sV -sC $1 -o $2.sVsC
 nmap -Pn -p- --max-retries 1 $1 -o $2.all
-nmap -Pn -p $(get_ports $2.all) $1 -o $2.sVsC.allports
+nmap -Pn -sV -sC -p $(get_ports $2.all) $1 -o $2.sVsC.allports
 
 echo "Done."
