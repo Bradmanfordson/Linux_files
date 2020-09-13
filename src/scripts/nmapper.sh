91f72f49a3e996cb6b0c4296e005048d54371351
Brad Roberts <bradr555@gmail.com>
2020-09-13 14:33:18 -0500

    Update nmapper.sh

--- src/scripts/nmapper.sh
+++ src/scripts/nmapper.sh
@@ -7,10 +7,24 @@ fi
 
 TARGET=$1
 OUTFILE=$2
-
+echo "####################"
+echo "# ----- FAST ----- #"
+echo "####################"
 nmap -Pn -F $1 -o $2.fast
+
+echo "#####################"
+echo "# ----- NORMAL -----#"
+echo "#####################"
 nmap -Pn $1 -o $2.normal
+
+echo "###################"
+echo "# ----- ALL ----- #"
+echo "###################"
 nmap -Pn -p- --max-retries 1 $1 -o $2.all
+
+echo "#######################"
+echo "# ----- ALL sVsC -----#"
+echo "#######################"
 nmap -Pn -sV -sC -p $(get_ports $2.all) $1 -oA $2.sVsC.allports
 
 echo "Done."
