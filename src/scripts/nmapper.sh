c156426394c1d4ae58e1790bcd5a325d62bd550c
Brad Roberts <bradr555@gmail.com>
2020-09-14 08:38:45 -0500

    Update nmapper.sh

--- src/scripts/nmapper.sh
+++ src/scripts/nmapper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This is my typical strategy for scanning boxes I'm pentesting.
+# I find this generally gives me all the information I need.
+
 if [ $# -lt 2 ]; then
         echo "Usage: $0 <ipaddress> <outfile>"
         exit 1
@@ -7,24 +10,25 @@ fi
 
 TARGET=$1
 OUTFILE=$2
+
 echo "####################"
 echo "# ----- FAST ----- #"
 echo "####################"
-nmap -Pn -F $1 -o $2.fast
+nmap -Pn -F $TARGET -o $OUTFILE.fast
 
 echo "#####################"
 echo "# ----- NORMAL -----#"
 echo "#####################"
-nmap -Pn $1 -o $2.normal
+nmap -Pn $TARGET -o $OUTFILE.normal
 
 echo "###################"
 echo "# ----- ALL ----- #"
 echo "###################"
-nmap -Pn -p- --max-retries 1 $1 -o $2.all
+nmap -Pn -p- --max-retries 1 $TARGET -o $OUTFILE.all
 
 echo "#######################"
 echo "# ----- ALL sVsC -----#"
 echo "#######################"
-nmap -Pn -sV -sC -p $(get_ports $2.all) $1 -oA $2.sVsC.allports
+nmap -Pn -sV -sC -p $(get_ports $OUTFILE.all) $TARGET -oA $OUTFILE.sVsC.allports
 
 echo "Done."
