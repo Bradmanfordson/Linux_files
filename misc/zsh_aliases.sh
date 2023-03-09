75862544bd05dde5901a6095745a4d8a28b4909b
ABradRoberts <bradr555@gmail.com>
2023-03-08 19:59:51 -0600

    :shipit:

--- misc/zsh_aliases.sh
+++ misc/zsh_aliases.sh
@@ -3,3 +3,6 @@ alias ...="cd ../.."
 alias python="python3"
 alias von="source venvy on"
 alias voff="source venvy off"
+alias update="sudo apt update && sudo apt full-upgrade -y; sudo updatedb"
+
+
